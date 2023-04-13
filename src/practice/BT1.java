00251a5684b5e098478264a1f671cc6ff478ca51
LSeolAh <snxwxh@gmail.com>
2023-04-13 14:04:24 +0900

    자료구조 이진트리

---
+++ src/practice/BT1.java
@@ -0,0 +1,34 @@
+package practice;
+class Node_B1{
+    char data;
+    //생성자 생성
+    Node_B1 right;
+    Node_B1 left;
+
+    public Node_B1 (char d){
+        data = d;
+    }
+}
+
+
+public class BT1 {
+    public static void main(String[] args){
+        //단말노드 메소드
+        Node_B1 g = new Node_B1('G');
+        Node_B1 h = new Node_B1('H');
+        Node_B1 f = new Node_B1('F');
+        //부모노드 메소드
+        Node_B1 d = new Node_B1('D');
+        d.right = g;
+        Node_B1 e = new Node_B1('E');
+        e.left = h;
+        Node_B1 c = new Node_B1('C');
+        c.right = f;
+        Node_B1 b = new Node_B1('B');
+        b.left = d;
+        b.right = e;
+        Node_B1 a = new Node_B1('A');
+        a.left = b;
+        a.right = c;
+    }
+}
